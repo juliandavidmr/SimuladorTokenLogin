c5fe51969d422b0371da119d3d6cd1ebbfc9c7ef
anlijudavid <anlijudavid@hotmail.com>
2016-09-21 20:06:45 -0500

    login

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/login/:url', function (req, res) {
-  console.log('=>', req.params);
-  res.redirect('http://' + req.params.url);
+router.get('/', (req, res) => {
+  res.render('index', { 
+    title: 'Simulador Login'
+  });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+var users = {
+  user: 'jul.mora@udla.edu.co',
+  pass: '123',
+  key: 'ksj23023s'
+};
+
+router.post('/sigin/:email/:pass/:url', (req, res) => {
+  console.log('===>', req.body);
+
+  var user = req.params.email;
+  var pass = req.params.pass;
+  var url = req.params.url;
+
+  console.log('Peticion');
+
+  if (users.pass === pass && users.user === user) {
+    console.log('Logeado');
+
+    res.redirect('http://' + url + '/?token=' + users.key);
+  } else {
+    res.json({
+      state: 'ERROR 100',
+      description: 'El usuario no existe'
+    });
+  }
+});
+
+router.get('/success/:token', (req, res) => {
+  res.json({
+    token: req.params.key
+  });
 });
 
 module.exports = router;
