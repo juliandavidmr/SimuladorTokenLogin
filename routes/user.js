30ee650eab326df355a9532bf0fab198f90594c3
anlijudavid <anlijudavid@hotmail.com>
2016-09-29 19:09:02 -0500

    mejoras en inicio de sesion

--- routes/user.js
+++ routes/user.js
@@ -7,31 +7,30 @@ var users = {
   key: 'ksj23023s'
 };
 
-router.post('/sigin/:email/:pass/:url', (req, res) => {
-  console.log('===>', req.body);
-
+router.get('/sigin/:email/:pass/:url', (req, res) => {
   var user = req.params.email;
   var pass = req.params.pass;
   var url = req.params.url;
 
-  console.log('Peticion');
-
+  console.log('Peticion: ', user, pass, url);
+  
+  if (!url) {
+    res.send("Se necesita URL de redirect");
+  }
   if (users.pass === pass && users.user === user) {
-    console.log('Logeado');
-
-    res.redirect('http://' + url + '/?token=' + users.key);
+    if (url) {
+      console.log('Logeado');
+      res.redirect('http://' + url + '/?token=' + users.key);
+    } else {
+      res.send("Se necesita URL de redirect");
+    }
   } else {
-    res.json({
-      state: 'ERROR 100',
-      description: 'El usuario no existe'
-    });
+    res.json({state: 'ERROR 100', description: 'El usuario no existe'});
   }
 });
 
 router.get('/success/:token', (req, res) => {
-  res.json({
-    token: req.params.key
-  });
+  res.json({token: req.params.key});
 });
 
 module.exports = router;
